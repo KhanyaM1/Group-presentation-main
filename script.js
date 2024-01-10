87045d7ee502354fe68fdb06a60db013b242b98d
EvelynYeboah <125741127+EvelynYeboah@users.noreply.github.com>
2024-01-10 23:16:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -32,37 +32,41 @@ function closeLoginModal() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+    updateDots();
+}
+
 function currentSlide(n) {
     showSlides(slideIndex = n);
     updateDots(); // Add this line to update dot styles
 }
-
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-
     if (n > slides.length) {
         slideIndex = 1;
     }
-
     if (n < 1) {
         slideIndex = slides.length;
     }
-
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-
     slides[slideIndex - 1].style.display = "block";
 }
-
 function updateDots() {
     var dots = document.getElementsByClassName("dot");
-
     for (var i = 0; i < dots.length; i++) {
         dots[i].classList.remove("active-dot");
     }
 
     dots[slideIndex - 1].classList.add("active-dot");
 }
+
+
+// Function the prev & next arrows
+document.querySelector(".next").addEventListener("click", function() {
+    plusSlides(1);
+});
